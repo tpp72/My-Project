e5ab4269706084c01b0b18c2ca4fb19b255e0692
Topong P. <nong.tickis@gmail.com>
2026-01-20 02:49:45 +0700

    - ?v=2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'SmartParking' }}</title>
-    <link rel="icon" href="{{ asset('favicon.ico?v=2') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
